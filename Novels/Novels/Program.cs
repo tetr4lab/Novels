aeba625906d408972878697015a4684259157d49
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2025-05-04 19:29:23 +0900

    起動できるところまで

--- Novels/Novels/Program.cs
+++ Novels/Novels/Program.cs
@@ -1,14 +1,78 @@
+using Microsoft.AspNetCore.Components.Server.Circuits;
+using MudBlazor;
+using MudBlazor.Services;
+using PetaPoco;
 using Novels.Client.Pages;
 using Novels.Components;
+using Tetr4lab;
 
 var builder = WebApplication.CreateBuilder (args);
+var connectionString = $"database=novels;{builder.Configuration.GetConnectionString ("Host")}{builder.Configuration.GetConnectionString ("Account")}Allow User Variables=true;";
 
 // Add services to the container.
 builder.Services.AddRazorComponents ()
     .AddInteractiveServerComponents ()
     .AddInteractiveWebAssemblyComponents ();
 
+// MudBlazor
+builder.Services.AddMudServices (config => {
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+    config.SnackbarConfiguration.MaximumOpacity = 80;
+});
+
+// クッキーとグーグルの認証を構成
+builder.Services.AddAuthentication (
+    builder.Configuration ["Authentication:Google:ClientId"]!,
+    builder.Configuration ["Authentication:Google:ClientSecret"]!
+);
+
+// メールアドレスを保持するクレームを要求する認可用のポリシーを構成
+await builder.Services.AddAuthorizationAsync (
+    $"database=accounts;{builder.Configuration.GetConnectionString ("Host")}{builder.Configuration.GetConnectionString ("Account")}Allow User Variables=true;",
+    new () {
+        { "Admin", "Administrator" },
+        { "Users", "Private" },
+    }
+);
+
+#if NET8_0_OR_GREATER
+// ページにカスケーディングパラメータ`Task<AuthenticationState>`を提供
+builder.Services.AddCascadingAuthenticationState ();
+#endif
+
+// 回路の閉鎖を検出するCircuitHandlerをセッション毎に使う
+builder.Services.AddScoped<CircuitHandler, CircuitClosureDetector> ();
+
+// PetaPoco with MySqlConnector
+builder.Services.AddScoped (_ => (Database) new MySqlDatabase (connectionString, "MySqlConnector"));
+
+// HTTP Client
+builder.Services.AddHttpClient ();
+
+// DataSet
+//builder.Services.AddScoped<NovelsDataSet> ();
+
 var app = builder.Build ();
+
+// Application level Culture
+app.UseRequestLocalization (new RequestLocalizationOptions ()
+    .SetDefaultCulture ("ja-JP")
+    .AddSupportedCultures (["ja-JP",])
+    .AddSupportedUICultures (["ja-JP",])
+);
+
+// Application Base Path
+var basePath = builder.Configuration ["AppBasePath"];
+if (!string.IsNullOrEmpty (basePath)) {
+    app.UsePathBase (basePath);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment ()) {
@@ -23,10 +87,13 @@ app.UseHttpsRedirection ();
 
 app.UseStaticFiles ();
 app.UseAntiforgery ();
+app.UseAuthentication ();
+app.UseAuthorization ();
 
 app.MapRazorComponents<App> ()
     .AddInteractiveServerRenderMode ()
     .AddInteractiveWebAssemblyRenderMode ()
     .AddAdditionalAssemblies (typeof (Novels.Client._Imports).Assembly);
 
+System.Diagnostics.Debug.WriteLine ("Initialized");
 app.Run ();
---
+++ Novels/Novels/wwwroot/js/NovelsUtilities.js
@@ -0,0 +1,26 @@
+﻿
+let modifierKeys = {
+    ctrl: false,
+    alt: false,
+    shift: false
+};
+
+document.addEventListener("keydown", (event) => {
+    modifierKeys.ctrl = event.ctrlKey;
+    modifierKeys.alt = event.altKey;
+    modifierKeys.shift = event.shiftKey;
+});
+
+document.addEventListener("keyup", (event) => {
+    modifierKeys.ctrl = event.ctrlKey;
+    modifierKeys.alt = event.altKey;
+    modifierKeys.shift = event.shiftKey;
+});
+
+function getModifierKeys() {
+    return modifierKeys;
+}
+
+function openInNewTab(url) {
+    window.open(url, '_blank');
+}
