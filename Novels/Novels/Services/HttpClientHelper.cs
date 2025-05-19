adbc3803f13e2ca9126a02c35d2773dbb9308a10
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2025-05-20 02:55:33 +0900

    書籍の更新をページから独立させる

--- Novels/Novels/Services/HttpClientHelper.cs
+++ Novels/Novels/Services/HttpClientHelper.cs
@@ -1,4 +1,7 @@
-﻿namespace Novels.Services;
+﻿using MudBlazor;
+using Novels.Data;
+
+namespace Novels.Services;
 public static class HttpClientHelper {
 
     /// <summary>クッキー付きでGetAsync</summary>
@@ -6,6 +9,35 @@ public static class HttpClientHelper {
         var request = new HttpRequestMessage (HttpMethod.Get, url);
         request.Headers.Add ("Cookie", string.Join ("; ", cookies.Select (c => $"{c.Key}={c.Value}")));
         return await client.SendAsync (request);
+    }
+
+
+    /// <summary>書籍の更新</summary>
+    public static async Task<List<string>> UpdateBookAsync (this HttpClient client, Book book) {
+        var result = new List<string> ();
+        if (book is not null) {
+            book.Html = null;
+            var cookies = new List<KeyValuePair<string, string>> { new ("over18", "yes"), };
+            using (var message = await client.GetWithCookiesAsync (book.Url, cookies)) {
+                if (message.IsSuccessStatusCode && message.StatusCode == System.Net.HttpStatusCode.OK) {
+                    var html = new List<string> { (book.Html = await message.Content.ReadAsStringAsync ()), };
+                    for (var i = 2; i <= book.LastPage; i++) {
+                        var url = $"{book.Url}{(book.Url.EndsWith ('/') ? "" : "/")}?p={i}";
+                        using (var message2 = await client.GetWithCookiesAsync (url, cookies)) {
+                            if (message2.IsSuccessStatusCode && message2.StatusCode == System.Net.HttpStatusCode.OK) {
+                                html.Add (await message2.Content.ReadAsStringAsync ());
+                            } else {
+                                result.Add ($"取得に失敗: {url} {message.StatusCode} {message.ReasonPhrase}");
+                            }
+                        }
+                    }
+                    book.Html = string.Join ('\n', html);
+                } else {
+                    result.Add ($"取得に失敗: {book.Url} {message.StatusCode} {message.ReasonPhrase}");
+                }
+            }
+        }
+        return result;
     }
 
 }
