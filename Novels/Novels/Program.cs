bcbd2963a50d4bb1b7b409840003b5c793ded6da
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2025-05-20 02:01:04 +0900

    Bookの更新を分離、DIの対象を`HttpClient`から`IHttpClientFactory`に変更してユーザーエージェントの変更を都度書かないように

--- Novels/Novels/Program.cs
+++ Novels/Novels/Program.cs
@@ -56,7 +56,9 @@ builder.Services.AddScoped<CircuitHandler, CircuitClosureDetector> ();
 builder.Services.AddScoped (_ => (Database) new MySqlDatabase (connectionString, "MySqlConnector"));
 
 // HTTP Client
-builder.Services.AddHttpClient ();
+builder.Services.AddHttpClient ("Novels", client => {
+    client.DefaultRequestHeaders.Add ("User-Agent", "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko");
+});
 
 // DataSet
 builder.Services.AddScoped<NovelsDataSet> ();
